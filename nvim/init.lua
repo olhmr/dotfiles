e44cbcf566dec5d41d26a3f2d1804e9fb88213c2
Olle Hammarstrom <olle.hammarstrom@rvu.co.uk>
2022-12-02 23:05:25 +0000

    feat: enable tabbing through fields in vsnip

--- nvim/init.lua
+++ nvim/init.lua
@@ -452,6 +452,13 @@ require('telescope').setup{
 -- vim-vsnip
 -------------------------------------------------------------------------------
 vim.g['vsnip_snippet_dir'] = '~/.config/nvim/.vsnip'
+-- jump forward or backward
+vim.cmd [[
+  imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+  smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+  imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+  smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+]]
 
 
 --:----------------------------------------------------------------------------
