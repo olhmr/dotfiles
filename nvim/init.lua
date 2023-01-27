67bac335854c4234586d31589bd3c625f801b7fe
Olle Hammarstrom <olle.hammarstrom@rvu.co.uk>
2023-01-27 18:05:06 +0100

    feat: only use explicitly selected items

--- nvim/init.lua
+++ nvim/init.lua
@@ -641,7 +641,7 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Only confirm explicitly selected items. Set `select` to `true` to accept currently selected item / the first item.
     ["<C-n>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
